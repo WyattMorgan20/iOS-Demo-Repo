102145bd33d084e2c6df4ca8f16d6029ccc248ae
Wyatt Morgan <iamtheyy@gmail.com>
2025-01-30 11:09:43 -0600

    Started initials app

---
+++ Classwork/InitialsApp/InitialsAppTests/InitialsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InitialsAppTests.swift
+//  InitialsAppTests
+//
+//  Created by Morgan,Wyatt J on 1/30/25.
+//
+
+import Testing
+@testable import InitialsApp
+
+struct InitialsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
