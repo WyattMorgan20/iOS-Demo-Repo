8e7946a6d27b0768c2d5a9cdd127cb4c0af634ca
Wyatt Morgan <iamtheyy@gmail.com>
2025-01-30 12:08:48 -0600

    added comment

--- Classwork/funcInClassExercise.playground/Contents.swift
+++ Classwork/funcInClassExercise.playground/Contents.swift
@@ -35,7 +35,7 @@ func initials2(_ fName: String, _ lName: String) -> String{
     let initialsOut2 = "\(fName.prefix(1)) \(lName.prefix(1))"
     return initialsOut2
 }
-
+// using "_ " when declaring the variables of a function allows you to call it without using variable names
 let initialsIn2 = initials2("Wyatt", "Morgan")
 print(initialsIn2)
 
