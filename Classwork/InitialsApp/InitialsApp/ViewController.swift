cbe889fbba3e0b7b28e20784fa7f660dbf99a49e
Wyatt Morgan <iamtheyy@gmail.com>
2025-01-30 11:26:18 -0600

    making initials app code

--- Classwork/InitialsApp/InitialsApp/ViewController.swift
+++ Classwork/InitialsApp/InitialsApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var fNameOT: UITextField!
+    
+    @IBOutlet weak var lNameOT: UITextField!
+    
+    @IBOutlet weak var outputOT: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func SubmitBTN(_ sender: UIButton) {
+        // read fName text and save it to variable fName
+        // read lName text and save it to variable lName
+        var fName: String = fNameOT.text!
+        var lName: String = lNameOT.text!
+        
+        // get the first letter of fName and lName
+        // MUST be uppercase
+        var fInitials = fName.prefix(1).uppercased()
+        var lInitials = lName.prefix(1).uppercased()
+        
+        // Display result in the outputOT
+        outputOT.text = "Your initials are \(fInitials) \(lInitials)"
+    }
+    
 }
 
