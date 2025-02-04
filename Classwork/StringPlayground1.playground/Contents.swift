041d51ce9d0ccb3884f8dcddcaa30edc67f33675
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-04 11:40:29 -0600

    added ws 2

--- Classwork/StringPlayground1.playground/Contents.swift
+++ Classwork/StringPlayground1.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// WS 1:
 var fact = "Swift is a type safe language"
 var dev = "Development of Swift began in 2010"
 var author = "Swift was created by Chris Lattner"
@@ -39,3 +40,43 @@ print(name[name.index(name.startIndex, offsetBy: 6)])
 
 // negative go from right to left, starts with 1
 print(name[name.index(name.endIndex, offsetBy: -6)])
+
+// WS 2:
+var shoppingList = "The shopping list contains: "
+var foodItems = "Cheese, Butter, Chocolate Spread"
+var clothes = "Socks, T-shirts"
+
+if clothes.hasPrefix("Socks") {
+    print("The first item in clothes is socks")
+}
+else{
+    print("socks is not the first item in clothes")
+}
+
+print(foodItems.split(separator: ","))
+
+if clothes.contains(",") {
+    print("Clothes contains more than one item")
+}
+else{
+    print("Clothes contain only one item")
+}
+
+foodItems[foodItems.startIndex..<foodItems.index(foodItems.endIndex,offsetBy: -7)]
+
+
+shoppingList += foodItems[foodItems.index(foodItems.startIndex, offsetBy: 8)..<foodItems.endIndex]
+
+
+clothes.remove(at: clothes.firstIndex(of: "T")!)
+
+
+clothes.remove(at: clothes.firstIndex(of: "-")!)
+
+
+print("\(shoppingList), \(clothes)")
+    
+clothes.insert(contentsOf: ", Trousers", at: clothes.endIndex)
+
+var firstIndexOfR = shoppingList.index(after: shoppingList.firstIndex(of: "r")!)
+print(shoppingList[..<firstIndexOfR])
