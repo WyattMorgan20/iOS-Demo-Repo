19d10b537898aa9f148eac5d6a35ba2f546423c0
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-06 11:31:10 -0600

    data structures practice

---
+++ Classwork/ArrayPractice.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var numbers: [Int] = [1, 4, 2, 9, 6, 8]
+
+numbers.sort()
+
+print(numbers)
+
+var oddNum: [Int] = [1, 3, 5, 7, 9]
+var evenNum: [Int] = [2, 4, 6, 8, 10]
+
+var num = [Int]()
+num.append(contentsOf: oddNum)
+num.append(contentsOf: evenNum)
+
+print(num)
+
+num.sort()
+print(num)
+
+num.reverse()
+print(num)
+
+var letters: [String] = ["B", "D", "C", "A"]
+letters.sort()
+print(letters)
+
+
