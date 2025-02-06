85b312ac1e2e017362b942dfd71e56a1f005326c
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-06 12:00:47 -0600

    Added to dictionaries

--- Classwork/DictionariesPractice.playground/Contents.swift
+++ Classwork/DictionariesPractice.playground/Contents.swift
@@ -3,9 +3,25 @@ import UIKit
 var movies = ["Tom Cruise" : "Mission Impossible", "Jeremy Renner" : "Mission Impossible", "Gal Gadot" : "Wonder Woman"]
 print(movies)
 
+movies.removeValue(forKey: "Gal Gadot")
+print(movies)
+
 var games = [1 : "Destiny 2", 2 : "Horizon: Forbidden West", 3 : "God of War Ragnarok"]
 print(games)
 
 print(games.count)
 
+games[4] = "Elden Ring Nightreign"
+print(games)
 
+for(key, values) in games{
+    print(values)
+}
+
+for(key, values) in games{
+    print(key)
+}
+
+for(key, values) in games{
+    print(key, values)
+}
