31554b857e4703bc86e66383624273d436d4148e
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-06 11:55:44 -0600

    added dictionaries practice

---
+++ Classwork/DictionariesPractice.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var movies = ["Tom Cruise" : "Mission Impossible", "Jeremy Renner" : "Mission Impossible", "Gal Gadot" : "Wonder Woman"]
+print(movies)
+
+var games = [1 : "Destiny 2", 2 : "Horizon: Forbidden West", 3 : "God of War Ragnarok"]
+print(games)
+
+print(games.count)
+
+
