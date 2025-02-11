dcb1b06efc56ba12079864ea45d934ccee215038
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-11 11:20:23 -0600

    started coursedisplayapp

---
+++ Classwork/CourseDisplayApp/CourseDisplayApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CourseDisplayApp
+//
+//  Created by Morgan,Wyatt J on 2/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageViewOL: UIImageView!
+    
+    @IBOutlet weak var crsNumOL: UILabel!
+    
+    @IBOutlet weak var crsTitleOL: UILabel!
+    
+    @IBOutlet weak var semOfferedOL: UILabel!
+    
+    @IBOutlet weak var prevBttnOL: UIButton!
+    
+    @IBOutlet weak var nextBtnOL: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func prevBtnClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func nextBtnClicked(_ sender: UIButton) {
+        
+    }
+    
+}
+
---
+++ Classwork/CourseDisplayApp/CourseDisplayAppTests/CourseDisplayAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourseDisplayAppTests.swift
+//  CourseDisplayAppTests
+//
+//  Created by Morgan,Wyatt J on 2/11/25.
+//
+
+import Testing
+@testable import CourseDisplayApp
+
+struct CourseDisplayAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
