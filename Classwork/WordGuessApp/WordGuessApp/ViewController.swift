4cff9c4906c754c1b65f769dbc411f7b6341aa40
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-13 11:31:43 -0600

    created actions and outlets

--- Classwork/WordGuessApp/WordGuessApp/ViewController.swift
+++ Classwork/WordGuessApp/WordGuessApp/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayOL: UILabel!
+    
+    @IBOutlet weak var hintOL: UILabel!
+    
+    @IBOutlet weak var inputOL: UITextField!
+    
+    @IBOutlet weak var checkOL: UIButton!
+    
+    @IBOutlet weak var messageOL: UILabel!
+    
+    @IBOutlet weak var playAgainOL: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func checkBTNClick(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func playAgainBTNClick(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func letterEntered(_ sender: UITextField) {
+        
+    }
 }
 
