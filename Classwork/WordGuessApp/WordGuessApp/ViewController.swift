4e5507543d538d09b8187b8d266a3551ae8ae64c
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-13 11:52:53 -0600

    started coding wordguessapp

--- Classwork/WordGuessApp/WordGuessApp/ViewController.swift
+++ Classwork/WordGuessApp/WordGuessApp/ViewController.swift
@@ -22,15 +22,67 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var playAgainOL: UIButton!
     
+    var words = [
+        ["Destiny", "Cursed Devs"],
+        ["Basenji", "Dog"],
+        ["Car", "Vehicle"],
+        ["Northwest", "School"]
+    ]
+    
+    var count = 0;
+    var word = ""
+    var lettersGuessed = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // Check button should be disabled at start
+        checkOL.isEnabled = false
+        
+        // Get first word from the array
+        word = words[count][0]
+        displayOL.text = ""
+        
+        // Populate the display label with the underscores.
+        // The # of underscores is equal to the # of characters in the word
+        updateUnderscores();
+        
+        // Get the first hint from the array
+        hintOL.text = words[count][1]
+        
+        // Clear the status label initially
+        messageOL.text = ""
         
     }
 
     @IBAction func checkBTNClick(_ sender: UIButton) {
+        // Get the text from the text field
+        var letter = inputOL.text!
         
+        // Replace the guessed letter if the letter is part of the word
+        lettersGuessed = lettersGuessed + letter
+        var revealedWord = ""
+        for item in word{
+            if lettersGuessed.contains(item){
+                revealedWord += "\(item)"
+            }
+            else{
+                revealedWord += "_ "
+            }
+        }
+        
+        // Assigning the word to dispayOL after a guess
+        displayOL.text = revealedWord
+        inputOL.text = ""
+        
+        // If the word is guessed cofrectly we are enabling the
+        // Play again button
+        if displayOL.text!.contains("_") == false{
+            playAgainOL.isHidden = false
+            checkOL.isEnabled = false
+        }
+        checkOL.isEnabled = false
     }
     
     @IBAction func playAgainBTNClick(_ sender: UIButton) {
@@ -40,5 +92,12 @@ class ViewController: UIViewController {
     @IBAction func letterEntered(_ sender: UITextField) {
         
     }
+    
+    func updateUnderscores(){
+        for letter in word{
+            displayOL.text! += "_ "
+        }
+    }
+    
 }
 
