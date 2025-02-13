aa08ff4c87c3ccde7edc4259541d3aa4c24db705
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-13 11:17:28 -0600

    added word guess app

---
+++ Classwork/WordGuessApp/WordGuessAppTests/WordGuessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGuessAppTests.swift
+//  WordGuessAppTests
+//
+//  Created by Morgan,Wyatt J on 2/13/25.
+//
+
+import Testing
+@testable import WordGuessApp
+
+struct WordGuessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
