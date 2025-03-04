9cb5c219f7836e98c301f4566d72580a857775dc
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-04 11:27:06 -0600

    added code to project

--- Classwork/Coordinates/Coordinates/ViewController.swift
+++ Classwork/Coordinates/Coordinates/ViewController.swift
@@ -18,7 +18,33 @@ class ViewController: UIViewController {
         // Set random image
         imageViewOT.image = UIImage(named: "Best")
         
-        let minx = imageViewOT.
+        let minx = imageViewOT.frame.minX
+        let miny = imageViewOT.frame.minY
+        print(minx, ",", miny)
+        
+        let maxx = imageViewOT.frame.maxX
+        let maxy = imageViewOT.frame.maxY
+        print(maxx, ",", maxy)
+        
+        // Move the location to the upper left corner.
+        imageViewOT.frame.origin.x = 0
+        imageViewOT.frame.origin.y = 0
+        
+        // Move location to upper right corner.
+        imageViewOT.frame.origin.x = 293
+        imageViewOT.frame.origin.y = 0
+        
+        // Move location to lower left corner.
+        imageViewOT.frame.origin.x = 0
+        imageViewOT.frame.origin.y = 752
+        
+        // Move location to lower right corner.
+        imageViewOT.frame.origin.x = 293
+        imageViewOT.frame.origin.y = 752
+        
+        // Move location to the center of the view.
+        imageViewOT.frame.origin.x = 146.5
+        imageViewOT.frame.origin.y = 376
     }
 
 
