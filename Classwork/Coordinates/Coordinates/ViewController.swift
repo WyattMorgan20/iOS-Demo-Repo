8ba8d60220c12419676bf119a804c9b51dc1d53d
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-04 11:11:48 -0600

    created coordinates project

---
+++ Classwork/Coordinates/Coordinates/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Coordinates
+//
+//  Created by Morgan,Wyatt J on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageViewOT: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Set random image
+        imageViewOT.image = UIImage(named: "Best")
+        
+        let minx = imageViewOT.
+    }
+
+
+}
+
---
+++ Classwork/Coordinates/CoordinatesTests/CoordinatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatesTests.swift
+//  CoordinatesTests
+//
+//  Created by Morgan,Wyatt J on 3/4/25.
+//
+
+import Testing
+@testable import Coordinates
+
+struct CoordinatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
