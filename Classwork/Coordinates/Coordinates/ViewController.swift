d5332cdbf84c6ae69192dac9be32df1ed9fc6daa
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-04 11:46:40 -0600

    finished coordinates project

--- Classwork/Coordinates/Coordinates/ViewController.swift
+++ Classwork/Coordinates/Coordinates/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // Set random image
-        imageViewOT.image = UIImage(named: "Best")
+        imageViewOT.image = UIImage(named: "download")
+        
+        let imagex = 176
+        let imagey = 117
         
         let minx = imageViewOT.frame.minX
         let miny = imageViewOT.frame.minY
@@ -31,20 +34,32 @@ class ViewController: UIViewController {
         imageViewOT.frame.origin.y = 0
         
         // Move location to upper right corner.
-        imageViewOT.frame.origin.x = 293
+        imageViewOT.frame.origin.x = 217
+        // adjust x to fit in the screen width (393 - imageViewOutlet.frame.width)
         imageViewOT.frame.origin.y = 0
+        // The screen width is 414 (iPhone 16 pro), and we subtract
+        // the image width (imageViewOT.frame.width) from it to ensure the
+        // image's right edge aligns with the right side of the screen.
         
         // Move location to lower left corner.
         imageViewOT.frame.origin.x = 0
-        imageViewOT.frame.origin.y = 752
+        imageViewOT.frame.origin.y = 735
+        // adjust y to fit in the screen height (852 - imageViewOutlet.frame.height)
+        // The screen height is 896 (iPhone 16 pro), and we subtract
+        // the image height (imageViewOT.frame.height) from it to ensure the
+        // image's bottom edge aligns with the bottom edge of the screen.
         
         // Move location to lower right corner.
-        imageViewOT.frame.origin.x = 293
-        imageViewOT.frame.origin.y = 752
+        imageViewOT.frame.origin.x = 217
+        // adjust x to fit the screen width i.e., (393 - imageViewOT.frame.width)
+        imageViewOT.frame.origin.y = 735
+        // adjust y to fit the screen height i.e., (852 - imageViewOT.frame.height)
         
         // Move location to the center of the view.
-        imageViewOT.frame.origin.x = 146.5
-        imageViewOT.frame.origin.y = 376
+        imageViewOT.frame.origin.x = 119
+        // adjust to center x i.e., ((414 - imageViewOT.frame.width) / 2)
+        imageViewOT.frame.origin.y = 389.5
+        // adjust to center y i.e., ((896 - imageViewOT.frame.width) / 2)
     }
 
 
