bd0c1fd78ab596dd246e585b3b7ebc4765a7b61a
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-18 11:23:27 -0500

    created animationDemo project

---
+++ Classwork/AnimationsDemo/AnimationsDemoTests/AnimationsDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsDemoTests.swift
+//  AnimationsDemoTests
+//
+//  Created by Morgan,Wyatt J on 3/18/25.
+//
+
+import Testing
+@testable import AnimationsDemo
+
+struct AnimationsDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
