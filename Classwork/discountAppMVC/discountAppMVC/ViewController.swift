23f94e124c908c8ceb273c29cb22903bcb11dbf3
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-20 11:24:28 -0500

    sstarted discountMVC app

---
+++ Classwork/discountAppMVC/discountAppMVC/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  discountAppMVC
+//
+//  Created by Morgan,Wyatt J on 3/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var amountOL: UITextField!
+    
+    @IBOutlet weak var discountOL: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func CalcBTN(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
---
+++ Classwork/discountAppMVC/discountAppMVCTests/discountAppMVCTests.swift
@@ -0,0 +1,17 @@
+//
+//  discountAppMVCTests.swift
+//  discountAppMVCTests
+//
+//  Created by Morgan,Wyatt J on 3/20/25.
+//
+
+import Testing
+@testable import discountAppMVC
+
+struct discountAppMVCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
