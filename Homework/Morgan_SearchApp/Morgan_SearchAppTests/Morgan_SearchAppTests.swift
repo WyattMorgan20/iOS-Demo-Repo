a78b98adb67840557cd5b02197a015b11dea9c4a
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-31 16:56:35 -0500

    created searchapp project

---
+++ Homework/Morgan_SearchApp/Morgan_SearchAppTests/Morgan_SearchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Morgan_SearchAppTests.swift
+//  Morgan_SearchAppTests
+//
+//  Created by Wyatt Morgan on 3/31/25.
+//
+
+import Testing
+@testable import Morgan_SearchApp
+
+struct Morgan_SearchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
