22d86914b284bcb9195836eca651362e2d9ffecf
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-01 11:21:11 -0500

    practice exam 02 created

---
+++ Classwork/Practice_Exam02/MorganWPracticeExam02/MorganWPracticeExam02/CalculatedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatedViewController.swift
+//  MorganWPracticeExam02
+//
+//  Created by Morgan,Wyatt J on 4/1/25.
+//
+
+import UIKit
+
+class CalculatedViewController: UIViewController {
+
+    @IBOutlet weak var activityResultOT: UILabel!
+    
+    @IBOutlet weak var durationResultOT: UILabel!
+    
+    @IBOutlet weak var cpmResultOT: UILabel!
+    
+    @IBOutlet weak var totalCaloriesBurnedOT: UILabel!
+    
+    @IBOutlet weak var imageOT: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Classwork/Practice_Exam02/MorganWPracticeExam02/MorganWPracticeExam02Tests/MorganWPracticeExam02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MorganWPracticeExam02Tests.swift
+//  MorganWPracticeExam02Tests
+//
+//  Created by Morgan,Wyatt J on 4/1/25.
+//
+
+import Testing
+@testable import MorganWPracticeExam02
+
+struct MorganWPracticeExam02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
