668a7b65d7477838d30cf715ecdd69666c928d79
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-01 16:03:08 -0500

    attempted implementing button enable settings

--- Homework/Morgan_SearchApp/Morgan_SearchApp/ViewController.swift
+++ Homework/Morgan_SearchApp/Morgan_SearchApp/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
     var topics_description = ["Famous landmarks around the world", "Powerful enemies in the Destiny 2 Universe", "Different gaming consoles", "Different game developers", "Shows based off video games"]
     
     var iteratorNum = 0
-    var topicIndex = 0
+    var topic = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,31 +48,30 @@ class ViewController: UIViewController {
     @IBAction func searchButtonAction(_ sender: UIButton) {
         AudioServicesPlaySystemSound(1113)
         var textInput = searchTextField.text!
-        var topic = 0
         
         if(famousLandmarks_keywords.contains(textInput)){
             topic = 1
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
             topicInfoText.text! = topics_description[0]
         }
         else if(destiny2Enemies_keywords.contains(textInput)){
             topic = 2
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
             topicInfoText.text! = topics_description[1]
         }
         else if(gameConsole_keywords.contains(textInput)){
             topic = 3
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
             topicInfoText.text! = topics_description[2]
         }
         else if(gameDev_keywords.contains(textInput)){
             topic = 4
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
             topicInfoText.text! = topics_description[3]
         }
         else if(gameShow_keywords.contains(textInput)){
             topic = 5
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
             topicInfoText.text! = topics_description[4]
         }
         
@@ -82,10 +81,10 @@ class ViewController: UIViewController {
         AudioServicesPlaySystemSound(1105)
         if(iteratorNum <= 2){
             iteratorNum += 1
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
         }
         
-        if(iteratorNum == 4){
+        if(iteratorNum == 2){
             sender.isEnabled = false;
         }
         else{
@@ -95,9 +94,9 @@ class ViewController: UIViewController {
     
     @IBAction func showPrevImageBtn(_ sender: UIButton) {
         AudioServicesPlaySystemSound(1105)
-        if(iteratorNum >= 2){
+        if(iteratorNum >= 0){
             iteratorNum -= 1
-            resultImage.image = UIImage(named: topicsArr[topicIndex][iteratorNum])
+            resultImage.image = UIImage(named: topicsArr[topic - 1][iteratorNum])
         }
         
         if(iteratorNum == 0){
