063b6f85ab956feef128180e599fbc5450c27361
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-10 11:23:22 -0500

    created collectionViewMovies project

---
+++ Classwork/CollectionViewMovies/CollectionViewMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewMovies
+//
+//  Created by Morgan,Wyatt J on 4/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Classwork/CollectionViewMovies/CollectionViewMoviesTests/CollectionViewMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewMoviesTests.swift
+//  CollectionViewMoviesTests
+//
+//  Created by Morgan,Wyatt J on 4/10/25.
+//
+
+import Testing
+@testable import CollectionViewMovies
+
+struct CollectionViewMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
