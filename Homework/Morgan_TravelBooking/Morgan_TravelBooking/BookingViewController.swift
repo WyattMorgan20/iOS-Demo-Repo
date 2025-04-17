75ed9ce5cfb99dc2ad11b3dace34e5e29080b4d8
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-17 14:47:00 -0500

    finished assignment 06

--- Homework/Morgan_TravelBooking/Morgan_TravelBooking/BookingViewController.swift
+++ Homework/Morgan_TravelBooking/Morgan_TravelBooking/BookingViewController.swift
@@ -15,6 +15,13 @@ class BookingViewController: UIViewController {
     
     @IBOutlet weak var cabinTypeOL: UITextField!
     
+    var trvlName = ""
+    var noOfTravellers = 0
+    var cabinType = ""
+    var totalCost: Int = 0
+    var imgName = ""
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,7 +29,41 @@ class BookingViewController: UIViewController {
 
 
     @IBAction func bookNowButton(_ sender: UIButton) {
+        trvlName = travellerNameOL.text!
+        noOfTravellers = Int(noOfTravellersOL.text!)!
+        cabinType = cabinTypeOL.text!
         
+        if cabinType.lowercased() == "economy" {
+            totalCost = noOfTravellers * 150
+            imgName = "economyIMG"
+            result = "Enjoy your Economy Trip!"
+        }
+        else if cabinType.lowercased() == "luxury" {
+            totalCost = noOfTravellers * 250
+            imgName = "luxuryIMG"
+            result = "Enjoy your Luxury Trip!"
+        }
+        else {
+            totalCost = 0
+            imgName = "invalidIMG"
+            result = "Please select a valid class"
+        }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var transition =  segue.identifier
+        if transition == "resultSegue" {
+            // create the destination as ResultVC
+            var destination = segue.destination as! MorganResultViewController
+            
+            destination.cabinTypeOT = cabinType
+            destination.imgNameOT = imgName
+            destination.trvlNameOT = trvlName
+            destination.noOfTravellersOT = noOfTravellers
+            destination.resultOT = result
+            destination.totalCostOT = totalCost
+        }
+    }
+    
 }
 
--- Homework/Morgan_TravelBooking/Morgan_TravelBooking/MorganResultViewController.swift
+++ Homework/Morgan_TravelBooking/Morgan_TravelBooking/MorganResultViewController.swift
@@ -21,10 +21,25 @@ class MorganResultViewController: UIViewController {
     
     @IBOutlet weak var resultOL: UILabel!
     
+    var trvlNameOT = ""
+    var noOfTravellersOT = 0
+    var cabinTypeOT = ""
+    var totalCostOT: Int = 0
+    var imgNameOT = ""
+    var resultOT = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imageOL.image = UIImage(named: imgNameOT)
+        travellerNameOL.text = travellerNameOL.text! + " " + trvlNameOT
+        noOfTravellersOL.text = noOfTravellersOL.text! + " \(noOfTravellersOT)"
+        cabinTypeOL.text = cabinTypeOL.text! + " " + cabinTypeOT
+        totalCostOL.text = totalCostOL.text! + " " + "$\(totalCostOT)"
+        resultOL.text = resultOT
+        
     }
     
 
